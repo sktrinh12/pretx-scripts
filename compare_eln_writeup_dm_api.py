f90e4609fde2159a54f5610bb17b684e3e72d1d3
sktrinh12 <strkinh12@gmail.com>
2025-01-24 15:50:24 -0500

    US194|change expire time of token

--- compare_eln_writeup_dm_api.py
+++ compare_eln_writeup_dm_api.py
@@ -26,7 +26,7 @@ DS_IDS = {
     "prelude-prod-sdpo-8251": {"proj_id": 98000, "exp_ids": 1403, "summary": 1404},
 }
 BASE_URL = "dotmatics.net/browser/api"
-EXPIRE = 200 #1.5*60*60
+EXPIRE = 1.5*60*60
 DB_POOL = None
 DB_CONFIG = {
     "dbname": getenv("DB_NAME"),
