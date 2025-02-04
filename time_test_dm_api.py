603ccfdcc73df6129564ec9c0ceb3faf4dea46e1
sktrinh12 <strkinh12@gmail.com>
2025-02-04 18:15:12 -0500

    US204|remove indent, need to take diff in time for proper output, i as index

--- time_test_dm_api.py
+++ time_test_dm_api.py
@@ -35,19 +35,15 @@ for i, exp_id in enumerate(reversed(list(exp_id_list))):
     writeup_url_endpoint = f"{browser_url}/studies/experiment/{exp_id}/writeup/{{includeHtml}}"
     response = requests.get(writeup_url_endpoint, headers={"Authorization": f"Dotmatics {token}"})
     eln_writeup = response.json()
-    # if type(eln_writeup) is str and "?" in eln_writeup:
-        # print(exp_id)
-        # print(eln_writeup)
 
-        exp_summary_endpoint = f"{browser_url}/data/testadmin/98000/1404_PROTOCOL,1404_PROTOCOL_ID,1404_ISID,1404_CREATED_DATE/{exp_id}"
-        response = requests.get(exp_summary_endpoint, headers={"Authorization": f"Dotmatics {token}"})
-        # print(response.json())
+    exp_summary_endpoint = f"{browser_url}/data/testadmin/98000/1404_PROTOCOL,1404_PROTOCOL_ID,1404_ISID,1404_CREATED_DATE/{exp_id}"
+    response = requests.get(exp_summary_endpoint, headers={"Authorization": f"Dotmatics {token}"})
 
-    iteration_end_time = time.time()  # End timing for this iteration
+    iteration_end_time = time.time()
     # print(f"{exp_id} took {iteration_end_time - iteration_start_time:.4f} seconds")
     with open(csv_filename, "a", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([index, exp_id, f"{fetch_time:.4f}"])
+        writer.writerow([i, exp_id, f"{iteration_end_time - iteration_start_time:.4f}"])
         csvfile.flush()
 
 script_end_time = time.time()
