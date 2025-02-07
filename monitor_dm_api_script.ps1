74b407e80a2b537eb8ae5bbf9d2aa85d41f7f5eb
sktrinh12 <strkinh12@gmail.com>
2025-02-07 13:38:35 -0500

    US206|create ps script to monitor python dm api script

---
+++ monitor_dm_api_script.ps1
@@ -0,0 +1,23 @@
+$scriptPath = "C:\Users\Disco-lx\Dotmatics%20Scripts\simulate_error.py"
+$logFile = "C:\Users\Disco-lx\dm_api_script_log.txt"
+
+$firstRun = $true
+
+while ($true) {
+    $cmdArgs = "-l 100000 -s 30"
+    if ($firstRun) {
+        $cmdArgs += " -c"
+    }
+
+    Write-Output "$(Get-Date) - Running: python $scriptPath $cmdArgs" | Out-File -Append $logFile
+    $process = Start-Process -FilePath "python" -ArgumentList "$scriptPath $cmdArgs" -NoNewWindow -PassThru -Wait
+
+    if ($process.ExitCode -ne 0) {
+        Write-Output "$(Get-Date) - Script failed, restarting..." | Out-File -Append $logFile
+        Start-Sleep -Seconds 10
+        $firstRun = $false
+    } else {
+        Write-Output "$(Get-Date) - Script completed successfully" | Out-File -Append $logFile
+        break
+    }
+}
