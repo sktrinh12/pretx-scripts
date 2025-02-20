fa0489d53de0be8e7a4246c4d636da4bd15e5c92
sktrinh12 <strkinh12@gmail.com>
2025-02-20 11:43:04 -0500

    US216|modularise text comparison ml fx to separate module file for compare_eln_writeup_dm_api

--- compare_eln_writeup_dm_api.py
+++ compare_eln_writeup_dm_api.py
@@ -25,14 +25,11 @@ import argparse
 import aiohttp
 import asyncio
 import asyncpg
-from difflib import unified_diff, SequenceMatcher
 import json
-import torch
 from time import sleep
 from datetime import date, datetime
-from transformers import AutoTokenizer, AutoModel
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from difflib import unified_diff, SequenceMatcher
+from ml_modules import scibert_compare, tfidf_compare
 
 
 load_dotenv(override=True)
@@ -54,9 +51,6 @@ DB_CONFIG = {
     "host": getenv("DB_HOST"),
     "port": getenv("DB_PORT"),
 }
-MODEL_NAME = "allenai/scibert_scivocab_uncased"
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-model = AutoModel.from_pretrained(MODEL_NAME)
 exp_id_list = []
 
 
@@ -304,53 +298,6 @@ async def fetch_write_up(exp_id: str, system_name: str, analysis_date: date):
             system_name,
             analysis_date,
         )
-
-
-def get_embedding(text):
-    """
-    Generate embedding for a given text using SciBERT.
-    """
-    inputs = tokenizer(
-        text, return_tensors="pt", truncation=True, padding=True, max_length=512
-    )
-    with torch.no_grad():
-        outputs = model(**inputs)
-        embedding = outputs.last_hidden_state[:, 0, :]  # CLS token embedding
-    return embedding
-
-
-def scibert_compare(text1, text2):
-    """
-    Compare two texts using scibert model followed by cosine similarity.
-
-    Pre-trained BERT: Good for general understanding including scientific texts.
-    SciBERT is trained on papers from the corpus of semanticscholar.org.
-    Corpus size is 1.14M papers, 3.1B tokens
-    """
-    try:
-        embedding1 = get_embedding(text1)
-        embedding2 = get_embedding(text2)
-        similarity = cosine_similarity(embedding1.numpy(), embedding2.numpy())
-        return similarity[0][0]
-    except Exception:
-        return 0
-
-
-def tfidf_compare(text1, text2):
-    """
-    Compare two texts using TF-IDF and cosine similarity.
-
-    TF-IDF: Measures word importance in documents.
-    Matrix of Vectors: Represents documents in a high-dimensional space.
-    Cosine Similarity: Measures similarity between these vectors.
-    """
-    try:
-        vectorizer = TfidfVectorizer()
-        tfidf_matrix = vectorizer.fit_transform([text1, text2])
-        similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-        return similarity[0][0]
-    except Exception:
-        return 0
 
 
 # async def process_exp_id(exp_id_chunk, semaphore, analysis_date_1, analysis_date_2):
